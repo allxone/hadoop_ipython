d89be928bad1cc2e30ccbb1c72cc10e1ccc02e98
allxone <allxone@hotmail.com>
2015-05-03 00:17:03 +0200

    first build

---
+++ spark_utils.py
@@ -0,0 +1,39 @@
+def get_spark(nameApp='nameApp', queueName='nameQueue', numExecutors='4', executorCores='1', executorMemory='2G'):
+
+    import os
+    import sys
+    import glob
+
+    SPARK_HOME = os.environ['SPARK_HOME']
+
+    sys.path.insert(0, os.path.join(SPARK_HOME, 'python'))
+
+    py4j = glob.glob(SPARK_HOME + '/python/lib/' + 'py4j-*-src.zip')[0].split("/")[-1]
+    sys.path.insert(0, os.path.join(SPARK_HOME, 'python/lib/' + py4j))
+    
+    from pyspark import SparkContext
+    from pyspark import SparkConf
+
+    conf = SparkConf()\
+        .setAppName(nameApp)\
+        .setMaster('yarn-client')\
+        .set("spark.yarn.queue", queueName)\
+        .set("spark.executor.instances", numExecutors)\
+        .set("spark.executor.cores", executorCores)\
+        .set("spark.executor.memory", executorMemory)
+    sc = SparkContext(conf=conf)
+    
+    return sc
+
+def auth(user, realm):
+
+    from subprocess import Popen, PIPE
+    import getpass
+
+    pwd = getpass.getpass()
+
+    kinit = '/usr/bin/kinit'
+    kinit_args = [kinit, '%s@%s' % (user, realm)]
+    kinit = Popen(kinit_args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    kinit.stdin.write('%s\n' % pwd)
+    kinit.wait()
